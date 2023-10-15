c8d8babe46b1cc011e90e30e3c8ca8afca7d2000
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-10-15 17:05:44 +0200

    function hideArticle added

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ changeBackground.addEventListener("click", () => {
     body.classList.toggle("body--white");
 } );
 
-function section {
 
-}
+const hideArticle = (article) => {
+    article.classList.add(".article--hidden");
+};
+
+const init = () => {
+    const articleTop = document.querySelector(".js-articleTop");
+    const articleBottom = document.querySelector(".js.articleBottom");
+    hideArticle(articleTop);
+    hideArticle(articleBottom);
+};
+
+init();
