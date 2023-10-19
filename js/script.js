7169a662d523ef351dfb4bc3d966bb3dbd8ebcf8
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-10-19 16:00:35 +0200

    functtion in JS added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
 console.log("Cześć!");
 
-let changeBackground = document.querySelector(".changeBackground");
-let body = document.querySelector(".body");
-
-changeBackground.addEventListener("click", () => {
+const changeBackground = (background) => {
     body.classList.toggle("body--white");
-} );
-
-
-const hideArticle = (article) => {
-    article.classList.add(".article--hidden");
 };
 
 const init = () => {
-    const articleTop = document.querySelector(".js-articleTop");
-    const articleBottom = document.querySelector(".js.articleBottom");
-    hideArticle(articleTop);
-    hideArticle(articleBottom);
+    const background = document.querySelector(".js-background");
+    const body = document.querySelector(".js-body");
+    changeBackground(background);
+    changeBackground(body);
 };
 
 init();
